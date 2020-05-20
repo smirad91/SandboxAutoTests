3964be0cd884adb26052aa3ecaf24b43d4a1f9ab
smirad91 <smirad91@gmail.com>
2020-05-20 21:49:31 +0200

    Test is functional but need refactoring

--- SandBox/Lib/functions.py
+++ SandBox/Lib/functions.py
@@ -4,10 +4,14 @@ from selenium import webdriver
 
 
 def open_use_case(driver, title):
-    useCaseList = driver.find_element_by_class_name("list-group").find_elements_by_tag_name("a")
+    wait_until(lambda: driver.find_element_by_class_name("list-group"),10)
+    print(title)
+    print("title je iznad")
+    useCaseList = driver.find_element_by_css_selector("div[class*='list-group']").find_elements_by_tag_name("a")
     for ucl in useCaseList:
         if title.lower() == ucl.text.lower():
             ucl.click()
+            print("otvori use case")
             break
 
 def does_exist_use_case(driver, title):
@@ -21,26 +25,57 @@ def does_exist_use_case(driver, title):
 
 def get_input(driver):
     #returns list
+    wait_until(lambda: driver.find_element_by_name("title"), 10)
     title = driver.find_element_by_name("title").get_attribute('value')
     description = driver.find_element_by_name("description").text
     expected_result = driver.find_element_by_name("expected_result").get_attribute('value')
     # stepParentElement = driver.find_element_by_css_selector("form[autocomplete='']")
     steps = driver.find_elements_by_css_selector("input[placeholder*='* Use case step']")
+    steps_list = []
     print(title)
     print(description)
     print(expected_result)
     for s in steps:
+        steps_list.append(s.get_attribute("value"))
         print(s.get_attribute('value'))
-    return [title, description, expected_result, steps]
+    return [title, description, expected_result, steps_list]
 
 def set_input(driver, input_list):
-    wait_until(lambda:driver.find_element_by_name("title"), 10)
+    wait_until(lambda: driver.find_element_by_name("title"), 10)
+    driver.find_element_by_name("title").clear()
     driver.find_element_by_name("title").send_keys(input_list[0])
+    driver.find_element_by_name("description").clear()
     driver.find_element_by_name("description").send_keys(input_list[1])
+    driver.find_element_by_name("expected_result").clear()
     driver.find_element_by_name("expected_result").send_keys(input_list[2])
     steps = driver.find_elements_by_css_selector("input[placeholder*='* Use case step']")
+    if len(steps) < len(input_list[3]):
+        #click on add step
+        add_number = len(input_list[3]) - len(steps)
+        add_step_btn = driver.find_element_by_class_name("addTestStep")
+        for i in range(add_number):
+            time.sleep(1)
+            add_step_btn.click()
+    elif len(steps) > len(input_list[3]):
+        #remove
+        remove_number = len(steps) - len(input_list[3])
+        remove_btns = driver.find_elements_by_css_selector("button[data-testid='delete_usecase_step_btn']")
+        for i in range(remove_number):
+            time.sleep(1)
+            remove_btns[i].click()
+    steps = driver.find_elements_by_css_selector("input[placeholder*='* Use case step']")
     for i, step in enumerate(input_list[3]):
+        steps[i].clear()
         steps[i].send_keys(step)
+
+def delete_use_case(driver, title):
+    open_use_case(driver, title)
+    time.sleep(2)
+    driver.find_element_by_css_selector("button[aria-label='delete-button']").click()
+    time.sleep(2)
+    driver.find_element_by_xpath("//button[text()='Delete']").click()
+    wait_until(lambda: driver.find_element_by_class_name("list-group"),10)
+
 
 def input_use_case_info(driver, title, dsc, er, steps):
     driver.find_element_by_name("title").send_keys(title)
@@ -55,6 +90,9 @@ def create_use_case(driver):
 
 def open_use_cases_page(driver):
     driver.find_element_by_xpath("//*[contains(text(),'{}')]".format("se cases")).click()
+
+def submit_use_case(driver):
+    driver.find_element_by_css_selector("button[type='submit']").click()
 
 def login(driver, username, password):
     try:
--- SandBox/Tests/EditUseCaseNoOfCharacters.py
+++ SandBox/Tests/EditUseCaseNoOfCharacters.py
@@ -1,8 +1,13 @@
+import time
+
 from SandBox.Lib.functions import start_browser, go_to, login, open_use_cases_page, does_exist_use_case, open_use_case, \
-    get_input, set_input, create_use_case
+    get_input, set_input, create_use_case, submit_use_case, delete_use_case
 
 username = "smirad91@gmail.com"
 password = "qasb2020cao$"
+test_case_data = ["fdfdfdfdf", "fdfd", "dsdfsdfds", ["samo jedan step", "dva", "tri"]]
+
+
 use_case_title = "Use case for automated test EditUseCaseNoOfCharacters"
 
 browser = start_browser()
@@ -12,14 +17,26 @@ go_to(browser, "https://qa-sandbox.apps.htec.rs/")
 login(browser, username, password)
 open_use_cases_page(browser)
 create_use_case(browser)
-#
-# if does_exist_use_case(browser, use_case_title):
-#     open_use_case(browser, use_case_title)
-# else:
-#     assert False, "Use case {} does not exist".format(use_case_title)
+set_input(browser, test_case_data)
+submit_use_case(browser)
 
-use_case_info = get_input(browser)
+open_use_case(browser, test_case_data[0])
+current_use_case_info = get_input(browser)
+browser.find_element_by_css_selector("a[href='/use-cases']").click()
+time.sleep(3)
+print(current_use_case_info)
+for i in range(4):
+    if(i==3):
+        for b in range(3):
+            current_use_case_info[i][b] = "This field previously had {} characters".format(len(current_use_case_info[i][b]))
+    else:
+        current_use_case_info[i] = "This field previously had {} characters".format(len(current_use_case_info[i]))
 
-set_input(browser, ["fdfdfdfdf", "fdfd", "dsdfsdfds", ["samo jedan step"]])
+open_use_case(browser, test_case_data[0])
+set_input(browser, current_use_case_info)
+
+submit_use_case(browser)
+
+delete_use_case(browser, current_use_case_info[0])
 
 
